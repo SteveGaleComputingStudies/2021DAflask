e91491882b0b55459daa56a76503b71dd3971e92
Steve Gale <steve.gale@computingstudies.net>
2021-08-16 10:09:46 +1000

    test ok demo 16/08/21

--- ex1b.py
+++ ex1b.py
@@ -1,15 +1,16 @@
 # Getting Started With Flask
 # exercise 1
 # run the python file (VScode - Run Python File in Terminal) amd open browser to http://localhost:5000/
+# https://tedboy.github.io/flask/generated/flask.escape.html
 
-from flask import Flask	
+from flask import Flask	, escape
 app = Flask(__name__) # Flask constructor
 
 
 # route - bind function to app path
 @app.route('/')	
-def ex1():
-	return 'Exercise 1 completed - Hello All'
+def ex1b():
+	return escape('<html><body><H1>Ex 1A</H1></body></html>')           # safe removal of control characters eg cross site scripting
 
 # run the app
 if __name__=='__main__':
